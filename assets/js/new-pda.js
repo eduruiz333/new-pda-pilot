d813bf01e3eb8fd961b16e13bd7f153dc4aaaca6
Marcos Ruiz <marcos.ruiz@certisign.com.br>
2023-08-04 18:13:20 -0300

    Finalização do header

--- assets/js/new-pda.js
+++ assets/js/new-pda.js
@@ -1,8 +1,10 @@
 const navbarToggler = document.querySelector('.navbar-toggler');
 const mainTitle = document.querySelector('.main-title');
 const loginArea = document.querySelector('.login-area');
+document.querySelector('.mobile-tit-menu').innerHTML = document.querySelector('.active').innerHTML
 
 navbarToggler.addEventListener('click', () => {
   mainTitle.classList.toggle('d-none');
   loginArea.classList.toggle('d-none');
 });
+
